3227458fab0c7fa946a906050462797fc6257c13
alaagharbi <ala.gharbi@esprit.tn>
2022-03-09 00:57:18 +0100

    cc

---
+++ migrations/Version20220305010616.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220305010616 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article ADD decription VARCHAR(255) NOT NULL, ADD tag VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE commentaire CHANGE article_id article_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article DROP decription, DROP tag');
+        $this->addSql('ALTER TABLE commentaire CHANGE article_id article_id INT NOT NULL');
+    }
+}
